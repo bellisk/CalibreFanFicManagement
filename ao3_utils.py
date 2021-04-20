3afb945af5e42b36dd9957fef55acfbe72394073
Rae Knowler <daisywheel22@gmail.com>
2021-04-20 08:59:03 +0200

    Start adding in command to get info about AO3 history and bookmarks

--- ao3_utils.py
+++ ao3_utils.py
@@ -14,3 +14,26 @@ def get_ao3_bookmark_urls(cookie, expand_series, max_count, user):
         for work_id in api.user.bookmarks_ids(max_count, expand_series)
     ]
     return set(urls)
+
+
+def get_ao3_bookmarked_works(cookie, expand_series, max_count, user):
+    if max_count == 0:
+        return set([])
+
+    api = AO3()
+    api.login(user, cookie)
+    return api.user.bookmarks(max_count, expand_series)
+
+
+def get_ao3_history_works(cookie, expand_series, max_count, user):
+    api = AO3()
+    api.login(user, cookie)
+    history = api.user.reading_history()
+    works = []
+    n = 0
+    while n < max_count:
+        works.append(next(history))
+        n += 1
+
+    return works
+
