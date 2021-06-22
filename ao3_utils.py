5318d133b3489f21e76ffd4d7a38dd0aaeb07f3d
Rae Knowler <daisywheel22@gmail.com>
2021-06-22 18:59:49 +0200

    Add util to get marked-for-later ids

--- ao3_utils.py
+++ ao3_utils.py
@@ -14,3 +14,16 @@ def get_ao3_bookmark_urls(cookie, expand_series, max_count, user):
         for work_id in api.user.bookmarks_ids(max_count, expand_series)
     ]
     return set(urls)
+
+
+def get_ao3_marked_for_later_urls(cookie, expand_series, max_count, user):
+    if max_count == 0:
+        return set([])
+
+    api = AO3()
+    api.login(user, cookie)
+    urls = [
+        "https://archiveofourown.org/works/%s" % work_id
+        for work_id in api.user.marked_for_later_ids(max_count, expand_series)
+    ]
+    return set(urls)
