7d2d09a51a14f7150ef96e225f62bb8c50272e62
Rae Knowler <daisywheel22@gmail.com>
2021-11-01 17:06:32 +0100

    Add util functions to get urls for user's works and gifts

--- src/ao3_utils.py
+++ src/ao3_utils.py
@@ -21,6 +21,40 @@ def get_ao3_bookmark_urls(
     return set(urls)
 
 
+def get_ao3_work_urls(
+    cookie, max_count, user, oldest_date
+):
+    if max_count == 0:
+        return set([])
+
+    api = AO3()
+    api.login(user, cookie)
+    urls = [
+        "https://archiveofourown.org/works/%s" % work_id
+        for work_id in api.user.work_ids(
+            max_count, oldest_date
+        )
+    ]
+    return set(urls)
+
+
+def get_ao3_gift_urls(
+    cookie, max_count, user, oldest_date
+):
+    if max_count == 0:
+        return set([])
+
+    api = AO3()
+    api.login(user, cookie)
+    urls = [
+        "https://archiveofourown.org/works/%s" % work_id
+        for work_id in api.user.gift_ids(
+            max_count, oldest_date
+        )
+    ]
+    return set(urls)
+
+
 def get_ao3_marked_for_later_urls(cookie, max_count, user, oldest_date):
     if max_count == 0:
         return set([])
