5abc6b3eef1c1f45d85347cd9953e68c6c2f7a9c
villainousfriend <vf@email.com>
2022-08-01 17:18:29 +0200

    Set grouped_search_terms on database

--- scripts/add_stories_to_multiple_series.py
+++ scripts/add_stories_to_multiple_series.py
@@ -4,12 +4,18 @@ from subprocess import PIPE, STDOUT, check_output
 # The AO3 metadata that we get from FanficFare includes these series keys as well as
 # "series". Since "series" is a default Calibre field, we can ignore it here.
 AO3_SERIES_KEYS = ["series00", "series01", "series02", "series03"]
-PATH = '--with-library "/home/rae/Calibre Fanfic Library"'
+PATH = '"/home/rae/Calibre Fanfic Library"'
+ADD_GROUPED_SEARCH_SCRIPT = '''from calibre.library import db
+
+db = db(%s).new_api
+db.set_pref("grouped_search_terms", {"allseries": ["series", "series00", "series01", "series02", "series03"]})
+print(db.pref("grouped_search_terms"))
+'''
 
 
 def check_or_create_extra_series_columns(path):
     res = check_output(
-        "calibredb custom_columns {}".format(path),
+        "calibredb custom_columns --with-library {}".format(path),
         shell=True,
         stderr=STDOUT,
         stdin=PIPE,
@@ -21,16 +27,31 @@ def check_or_create_extra_series_columns(path):
     print(set(AO3_SERIES_KEYS))
     if set(columns).intersection(AO3_SERIES_KEYS) == set(AO3_SERIES_KEYS):
         print("Custom AO3 series columns are in Calibre Library")
-        return
+    else:
+        print("Adding custom AO3 series columns to Calibre library")
+        for c in AO3_SERIES_KEYS:
+            check_output(
+                "calibredb add_custom_column --with-library {} {} {} series".format(path, c, c),
+                shell=True,
+                stderr=STDOUT,
+                stdin=PIPE,
+            )
 
-    print("Adding custom AO3 series columns to Calibre library")
-    for c in AO3_SERIES_KEYS:
-        check_output(
-            "calibredb add_custom_column {} {} {} series".format(path, c, c),
-            shell=True,
-            stderr=STDOUT,
-            stdin=PIPE,
-        )
+    print("Adding grouped search term 'allseries' to Calibre Library")
+    _add_grouped_search_terms(path)
+
+
+def _add_grouped_search_terms(path):
+    # Add a new grouped search term "allseries" so that Calibre can search across all
+    # the series columns.
+    script = ADD_GROUPED_SEARCH_SCRIPT % path
+    res = check_output(
+        "calibre-debug -c '{}'".format(script),
+        shell=True,
+        stderr=STDOUT,
+        stdin=PIPE,
+    )
+    print(res)
 
 
 if __name__ == "__main__":
