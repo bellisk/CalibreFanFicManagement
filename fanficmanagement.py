326e98b5cac5c28a748eb1e3f67653c86a260f86
Rae Knowler <daisywheel22@gmail.com>
2024-04-12 13:45:53 +0200

    feat: Improve response when there is a problem with the options

--- fanficmanagement.py
+++ fanficmanagement.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import locale
+import sys
 
 from src.analyse import analyse
 from src.download import download
@@ -10,7 +11,11 @@ from src.options import set_up_options
 if __name__ == "__main__":
     # The locale for AO3, for converting formatted numbers.
     locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
-    command, options = set_up_options()
+    try:
+        command, options = set_up_options()
+    except ValueError as e:
+        sys.exit(e)
+
     permitted_commands = {"download": download, "analyse": analyse}
 
     eval(command + "(options)", permitted_commands, {"options": options})
