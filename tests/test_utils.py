ab6651aa1f3119a8043ef8f3fd7d28df76ed352f
Rae Knowler <daisywheel22@gmail.com>
2024-04-14 19:06:26 +0200

    tests: Test different cookie combinations

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,12 @@
 import os.path
 import time
-from unittest.mock import patch
+from argparse import Namespace
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
 
 from src import utils
+from src.exceptions import InvalidConfig
 
 test_filepath = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "fixtures", "test_files"
@@ -11,6 +15,21 @@ test_filepath = os.path.join(
 
 def mocked_localtime():
     return time.struct_time((2024, 4, 13, 9, 0, 0, 5, 104, 1))
+
+
+def mock_load_cookie(domain_name=None):
+    cookie = Mock()
+    cookie.name = "_otwarchive_session"
+    cookie.value = "test_browser_cookie"
+    jar = MagicMock()
+    jar.__iter__.return_value = [cookie]
+    return jar
+
+
+def mock_load_no_cookie(domain_name=None):
+    jar = MagicMock()
+    jar.__iter__.return_value = []
+    return jar
 
 
 def test_log_no_color():
@@ -65,3 +84,56 @@ def test_get_files_fileteype_fullpath():
     files = utils.get_files(test_filepath, filetype="csv", fullpath=True)
 
     assert sorted(files) == [os.path.join(test_filepath, "test.csv")]
+
+
+def test_setup_login_cookie(capsys):
+    options = Namespace(cookie="testcookie", use_browser_cookie=False)
+    utils.setup_login(options)
+
+    captured = capsys.readouterr()
+    assert "Using the cookie value you passed in" in captured.out
+
+    assert options.cookie == "testcookie"
+
+
+@patch("src.utils.browser_cookie3.load", mock_load_cookie)
+def test_setup_login_use_browser_cookie(capsys):
+    options = Namespace(cookie=None, use_browser_cookie=True)
+    utils.setup_login(options)
+
+    captured = capsys.readouterr()
+    assert "Found _otwarchive_session cookie from the browser" in captured.out
+
+    assert options.cookie == "test_browser_cookie"
+
+
+@patch("src.utils.browser_cookie3.load", mock_load_cookie)
+def test_setup_login_cookie_and_use_browser_cookie(capsys):
+    options = Namespace(cookie="testcookie", use_browser_cookie=True)
+    utils.setup_login(options)
+
+    captured = capsys.readouterr()
+    assert "Found _otwarchive_session cookie from the browser" in captured.out
+
+    assert options.cookie == "test_browser_cookie"
+
+
+@patch("src.utils.browser_cookie3.load", mock_load_no_cookie)
+def test_setup_login_use_browser_cookie_but_browser_cookie_not_found(capsys):
+    options = Namespace(cookie=None, use_browser_cookie=True)
+
+    with pytest.raises(
+        InvalidConfig, match="Tried to get the _otwarchive_session cookie"
+    ):
+        utils.setup_login(options)
+
+
+@patch("src.utils.browser_cookie3.load", mock_load_no_cookie)
+def test_setup_login_cookie_and_use_browser_cookie_but_browser_cookie_not_found(capsys):
+    options = Namespace(cookie="testcookie", use_browser_cookie=True)
+    utils.setup_login(options)
+
+    captured = capsys.readouterr()
+    assert "Falling back to the cookie value you passed in" in captured.out
+
+    assert options.cookie == "testcookie"
