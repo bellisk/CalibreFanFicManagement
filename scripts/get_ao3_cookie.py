666e594b4a2926290431d68d7e92c016c5549189
Rae Knowler <daisywheel22@gmail.com>
2025-10-12 13:34:21 +0200

    feat: Get cookie from Vivaldi instead of Firefox

--- scripts/get_ao3_cookie.py
+++ scripts/get_ao3_cookie.py
@@ -1,6 +1,6 @@
 import browser_cookie3
 
-cj = browser_cookie3.firefox(domain_name="archiveofourown.gay")
+cj = browser_cookie3.vivaldi(domain_name="archiveofourown.gay")
 for c in cj:
     if c.name == "_otwarchive_session":
         print(c.value)
